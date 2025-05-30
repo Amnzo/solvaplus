ccb35298d096ed201fcd89eb451af3efa52ccf2b
mohamed.salmi <mohamed.salmi@ebitda.tech>
2025-05-30 01:41:56 +0100

    30 mai 01:41h

--- SHERLY/views.py
+++ SHERLY/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from django.db.models import Avg, Count
 from django.core.mail import send_mail
 from sherly_app.models import Produit, Famille, Societe
